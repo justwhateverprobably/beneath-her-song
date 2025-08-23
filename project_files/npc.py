6336d0b21a1771ae7adabf84f865785616af9166
Aspen Arnott <00jaspen@gmail.com>
2025-08-23 09:35:00 -0700

    reorganized file path, made minor bug fixes, changed some descriptions slightly

--- project_files/npc.py
+++ project_files/npc.py
@@ -32,7 +32,7 @@ class NPC():
             NPCType.sailor: NPC("sailor", NPCType.sailor, "Broad and weathered, with a crooked nose, short grey hair, and tattered clothes, he seems paranoid, twitchy, and always his one good eye is always darting from place to place.", True),
             NPCType.hermit: NPC("hermit", NPCType.hermit, "He is a short, wiry, suntanned old man with a bald head and scraggly beard. His eyes look sunken and shadowed, making him look almost ancient. He mutters slowly to himself, as if time bends around him.", True),
             NPCType.bartender: NPC("bartender", NPCType.bartender, "He is a thick-built man, with faded tattoos covering his head and a gruff look on his face. He is calm, quiet, and always seems to be busy doing various tasks, or exchanging small talk.",  True),
-            NPCType.marla: NPC("Marla", NPCType.marla, "She's young, with thin sharp eyes, wrapped in an oversized coat with unevenly cut hair. Her appearance seems a bit odd, although looking around you notice that many other people here do too.", True)
+            NPCType.marla: NPC("Marla", NPCType.marla, "She's young, no older than 17 by the looks of it, with thin sharp eyes, and unevenly cut jet black hair. She's wrapped in an oversized coat with baggy pants, and a scarf wrapped around her neck. Her appearance seems a bit odd, although looking around you notice that many people here do.", True)
         }
     
     @staticmethod
